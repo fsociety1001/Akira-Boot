b46c2da3839931673e6a858297cd6babc117f9c0
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-10 22:01:05 -0300

    run.sh

---
+++ test/run.sh
@@ -0,0 +1,17 @@
+cd $(dirname $0)
+
+cd ..
+
+./mvnw clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
